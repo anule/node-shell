7452acb3ef0864bc40140d179e4a5ef80cfd4cd9
Anule Ndukwu <anule.ndukwu@gmail.com>
2017-09-18 16:11:51 -0400

    close attempt to build 'pipe'

--- bash.js
+++ bash.js
@@ -26,8 +26,9 @@ var command = require('./command.js');
 var done = function(output){
   process.stdout.write(output);
   if (cmdArrList.length > 1) {
-    if (cmdArrList[1].includes(' ')) {
-      
+    if (cmdArrList[1].includes(' ')) {//[cat bash.js, head]
+      var headArg = cmdArrList[1].split(' ');
+      command[headArg[0]](headArg[1], done);
     }
     command[cmdArrList[1]](output, fileArr, done);
   }
@@ -38,8 +39,8 @@ process.stdout.write('prompt > ');
 
 //the stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function(data){
-  var cmd = data.toString().trim();
-  var cmdArrList = cmd.split(/\s*\|\s*/g);
+  var cmd = data.toString().trim();// cmd = cat bas.js| head
+  var cmdArrList = cmd.split(/\s*\|\s*/g);// [cat bash.js, head]
   var cmdArr = cmdArrList[0].split(' ');  //[cat, bash.js]
   command[cmdArr[0]](cmdArr.slice(1), done); //command[cat](bash.js)
 
